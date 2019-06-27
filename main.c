792dc7fcba73717320c4d67bfe09d51b6a942fd4
Pavlo Rozbytskyi <rozbich15@gmail.com>
2019-06-27 20:55:17 +0200

    integrated reading from partition table and blocks

--- main.c
+++ main.c
@@ -1,32 +1,78 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
+#include "main.h"
 
-// a) Falscher Aufruf des Programms: Exit-Code 1. 
-// b) Image-Datei nicht gefunden: Exit-Code 2. 
-// c) Datei Ein/Ausgabefehler: Exit-Code 3. 
-// d) Illegale Partitionsnummer: Exit-Code 4. 
-// e) Partition enthaelt kein EOS32-Dateisystem: Exit-Code 5. 
-// f) Erfolgloser Aufruf von malloc(): Exit-Code 6. 
-// g) Alle anderen Fehler: Exit-Code 9
-
-#define INCORRECT_START 1
-#define IMAGE_NOT_FOUND 2
-#define FILE_IO_ERROR 3
-#define ILLEGAL_PARTITION 4
-#define NO_FILE_SYSTEM 5
-#define MEMORY_ALLOC_ERROR 6
-#define RANDOM_ERROR 9
+unsigned char *partition = "/home/przb86/Desktop/bs-task2/build/run/disk.img";
+unsigned int fsStart;
 
 int main(int argc, char *argv[]){
+	FILE *disk;
+
+	unsigned char partTable[SECTOR_SIZE];
+	unsigned char blockBuffer[BLOCK_SIZE];
+	unsigned char *ptptr;
+	unsigned int fsSize;
+	unsigned int partType;
+	int part;
+
 	if(argc < 3){
+		fprintf(stderr, "incorrect program: \"%s\" start\n", argv[1]);
 		exit(INCORRECT_START);
 	}
 
 	if(access(argv[1], F_OK) == -1){
+		fprintf(stderr, "file \"%s\" does not exist", argv[1]);
 		exit(IMAGE_NOT_FOUND);
 	}
 
-	
+	disk = fopen(argv[1], "rb");
+  	if (disk == NULL) {
+    	fprintf(stderr, "cannot open disk image file '%s', %d", partition, errno);
+		exit(IO_ERROR);
+  	}
+
+	char *endptr;
+	/* argv[2] is partition number of file system */
+    part = strtoul(argv[2], &endptr, 10); // parsing number 
+    if (*endptr != '\0' || part < 0 || part > 15) {
+      	fprintf(stderr, "illegal partition number '%s'", argv[2]);
+		exit(ILLEGAL_PARTITION);
+    }
+  	// setting file pointer to partition table
+	fseek(disk, 1 * SECTOR_SIZE, SEEK_SET); 
+    if (fread(partTable, 1, SECTOR_SIZE, disk) != SECTOR_SIZE) {
+        fprintf(stderr, "cannot read partition table of disk '%s'", argv[1]);
+	    exit(IO_ERROR);
+    }
+	// pointer to needed partition
+	ptptr = partTable + part * 32;
+    partType = get4Bytes(ptptr + 0);
+    if ((partType & 0x7FFFFFFF) != 0x00000058) {
+        fprintf(stderr, "partition %d of disk '%s' does not contain an EOS32 file system",
+            part, argv[1]);
+		exit(NO_FILE_SYSTEM);
+    }
+    fsStart = get4Bytes(ptptr + 4);
+    fsSize = get4Bytes(ptptr + 8);
+	// readBlock(disk, 1, blockBuffer);
+
 	return 0;
+}
+
+void readBlock(FILE *disk, EOS32_daddr_t blockNum, unsigned char *blockBuffer) {
+  fseek(disk, fsStart * SECTOR_SIZE + blockNum * BLOCK_SIZE, SEEK_SET);
+
+  if (fread(blockBuffer, BLOCK_SIZE, 1, disk) != 1) {
+    fprintf(stderr, "cannot read block %lu (0x%lX)", blockNum, blockNum);
+	exit(IO_ERROR);
+  }
+}
+
+unsigned int get4Bytes(unsigned char *addr) {
+  return (unsigned int) addr[0] << 24 |
+         (unsigned int) addr[1] << 16 |
+         (unsigned int) addr[2] <<  8 |
+         (unsigned int) addr[3] <<  0;
 }
---
+++ main.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define INCORRECT_START 1
+#define IMAGE_NOT_FOUND 2
+#define IO_ERROR 3
+#define ILLEGAL_PARTITION 4
+#define NO_FILE_SYSTEM 5
+#define MEMORY_ALLOC_ERROR 6
+#define RANDOM_ERROR 9
+
+// a) Falscher Aufruf des Programms: Exit-Code 1. 
+// b) Image-Datei nicht gefunden: Exit-Code 2. 
+// c) Datei Ein/Ausgabefehler: Exit-Code 3. 
+// d) Illegale Partitionsnummer: Exit-Code 4. 
+// e) Partition enthaelt kein EOS32-Dateisystem: Exit-Code 5. 
+// f) Erfolgloser Aufruf von malloc(): Exit-Code 6. 
+// g) Alle anderen Fehler: Exit-Code 9
+
+#define SECTOR_SIZE	512	/* disk sector size in bytes */
+#define BLOCK_SIZE	4096	/* disk block size in bytes */
+
+typedef unsigned int EOS32_ino_t;
+typedef unsigned int EOS32_daddr_t;
+typedef unsigned int EOS32_off_t;
+typedef int EOS32_time_t;
+
+void readBlock(FILE *, EOS32_daddr_t, unsigned char *);
+unsigned int get4Bytes(unsigned char *);
